d6236c7d4f2b1afd47ffc0322ad18090662a3505
kasara614 <kasera1996@gmail.com>
2025-01-09 18:35:18 +0530

    Wrapping-yt

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
 
@@ -11,14 +12,20 @@ const WatchPage = () => {
 
 
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
 
   return (
-    <div className="flex flex-col">
-      <div className="px-5 mt-16">
-        <iframe className="rounded-2xl" width="1000" height="600" src={"https://www.youtube.com/embed/" + searchParams.get("v")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <div className="flex flex-col w-full">
+      <div className="px-5 mt-16 flex w-full">
+        <div className="">
+          <iframe className="rounded-2xl" width="1000" height="600" src={"https://www.youtube.com/embed/" + searchParams.get("v")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
+        <div className=" w-full">
+          <LiveChat />
+        </div>
       </div>
       <CommentsContainer />
     </div>
